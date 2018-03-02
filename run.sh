30ef7847121eafe03560850b20d10728fd81dfc7
Jonathan Rockway <jon@jrock.us>
2018-03-02 22:41:49 +0000

    setup gpio for input

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ set -e
 set -x
 
 make
-sudo sh -c 'echo stop > /sys/class/remoteproc/remoteproc1/state'
+sudo sh -c 'echo stop > /sys/class/remoteproc/remoteproc1/state' || echo 'not able to stop'
 sudo cp -v gen/pru.out /lib/firmware/am335x-pru0-fw
 sudo config-pin P9_31 pruout
-sudo config-pin P9_29 pruout
+sudo config-pin P9_29 pruin
 sudo sh -c 'echo start > /sys/class/remoteproc/remoteproc1/state'
