1f5e2853655154fbb1df34f24231194f8f0eec1e
Jonathan Rockway <jon@jrock.us>
2018-03-02 22:40:34 +0000

    c program to time how quickly we can copy one gpio to another

--- copy_pin.c
+++ copy_pin.c
@@ -3,18 +3,16 @@
 #include "resource_table_empty.h"
 
 volatile register uint32_t __R30;
+volatile register uint32_t __R31;
 
 void main(void)
 {
-  __R30 = 0x0002;
-          
   /* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
   CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 
+  __R30 = 0x0000;
+  
   while (1) {
-    __R30 = 0x0001;
-    __delay_cycles(7);
-    __R30 = 0x0000;
-    __delay_cycles(10);
+    __R30 = 0x0001 & ((__R31 & 2) >> 1); // copy gpio 1 state to gpio 0
   }
 }
