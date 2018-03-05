aec062f24c513f796142b31c050f4fbafd084030
Jonathan Rockway <jon@jrock.us>
2018-03-05 11:46:47 +0000

    move PRU code to its own directory

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 set -e
 set -x
 
-make
+make -C pru
 sudo sh -c 'echo stop > /sys/class/remoteproc/remoteproc1/state' || echo 'not able to stop'
-sudo cp -v gen/pru.out /lib/firmware/am335x-pru0-fw
+sudo cp -v pru/gen/pru.out /lib/firmware/am335x-pru0-fw
 sudo config-pin P9_31 pruout
 sudo config-pin P9_29 pruin
 sudo sh -c 'echo start > /sys/class/remoteproc/remoteproc1/state'
-sudo sh -c 'echo 'a' > /dev/rpmsg_pru30'
+sudo sh -c 'echo a > /dev/rpmsg_pru30'
 sudo socat - /dev/rpmsg_pru30
